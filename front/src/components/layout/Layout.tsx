0f6742b299fad17279cfd929c362ce3b7e88b36a
ekkk1230 <ek10314@naver.com>
2026-05-08 11:58:12 +0900

    모달 폴더 수정

--- front/src/components/layout/Layout.tsx
+++ front/src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import * as S from './Layout.styles'
 import { IoMdArrowBack } from "react-icons/io";
 import { useNavigate } from 'react-router-dom';
 import BottomNav from './BottomNav';
-import Popup from '../../features/popup/Popup';
+import Popup from '../modal/Modal';
 import { useUiStore } from '../../store/useUiStore';
 
 interface LayoutProps {
